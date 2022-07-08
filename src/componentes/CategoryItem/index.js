7049527f8c828dd3d976afee1ab4ef7089ced6e5
Lucas Teixeira <sobrinho1@gmail.com>
2022-07-08 09:50:45 -0300

    treinamento mobile

---
+++ src/componentes/CategoryItem/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Image, StyleSheet, Text } from "react-native";
+import { TouchableOpacity } from "react-native-web";
+
+import { useNavigation } from "@react-navigation/native";
+
+export function CategoryItem({ data }) {
+    const navigation = useNavigation();
+
+    function handleNavigate() {
+        navigation.navigate("Category", { id: data.id, title: data?.attributes?.name });
+    }
+
+    return (
+        <TouchableOpacity style={styles.container} activeOpacity={0.9} onPress={handleNavigate}>
+            <Image
+                style={styles.icon}
+                source={{ url: `http://192.168.91.36:1337${data?.attibutes?.icon?.data?.attributes?.url}` }}
+            ></Image>
+
+            <Text style={styles.name}>{data?.attibutes?.name}</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#fff",
+        marginLeft: 8,
+        marginVertical: 8,
+        borderRadius: 8,
+        alignItems: "center",
+        justifyContent: "center",
+        paddingHorizontal: 20,
+    },
+    icon: {
+        width: 40,
+        height: 40,
+    },
+});
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,31 +1,43 @@
-import React, { useEffect } from "react";
-import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, FlatList } from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
 import { Feather } from "@expo/vector-icons";
 import api from "../../services/api";
+import { CategoryItem } from "../../componentes/CategoryItem";
 
 export default function Home() {
     const navigation = useNavigation();
+    const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-        async function loadData(){
-            const categories = await api.get('/api/categories?populate=icon');
+        async function loadData() {
+            const category = await api.get("/api/categories?populate=icon");
+            console.log(category);
+            setCategories(category.data.data);
         }
         loadData();
-    }, [])
-
-    
+    }, []);
 
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.header}>
                 <Text style={styles.name}>DevBlog</Text>
 
-                <TouchableOpacity onPress={() => navigation.navigate('Search')}>
+                <TouchableOpacity onPress={() => navigation.navigate("Search")}>
                     <Feather name="search" size={24} color="#FFF" />
                 </TouchableOpacity>
             </View>
+
+            <FlatList
+                showsHorizontalScrollIndicator={false}
+                horizontal={true}
+                contentContainerStyle={{ paddingRight: 12 }}
+                style={styles.categories}
+                data={categories}
+                keyExtractor={(item) => String(item.id)}
+                renderItem={({ item }) => <CategoryItem data={item} />}
+            ></FlatList>
         </SafeAreaView>
     );
 }
@@ -47,5 +59,12 @@ const styles = StyleSheet.create({
         fontSize: 28,
         color: "#FFF",
         fontWeight: "bold",
+    },
+    categories: {
+        maxHeight: 115,
+        backgroundColor: "#efefef",
+        marginHorizontal: 18,
+        borderRadius: 8,
+        zIndex: 9,
     },
 });
